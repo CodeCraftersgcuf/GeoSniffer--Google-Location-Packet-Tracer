741db9102b72207ec90a73857044115800f27389
CodeCraftersgcuf <abubakarsajjad.202202655@gcuf.edu.pk>
2024-04-04 16:51:11 +0500

    init commit
    First Push to main.

---
+++ main.py
@@ -0,0 +1,65 @@
+import dpkt
+import socket
+import pygeoip
+
+gi = pygeoip.GeoIP('GeoLiteCity.dat')
+
+    
+
+def plotIPs(pcap):
+    kmlPts = ''
+    for (ts, buf) in pcap:
+        try:
+            eth = dpkt.ethernet.Ethernet(buf)
+            ip = eth.data
+            src = socket.inet_ntoa(ip.src)
+            dst = socket.inet_ntoa(ip.dst)
+            KML = retKML(dst, src)
+            kmlPts = kmlPts + KML
+        except:
+            pass
+    return kmlPts
+# Add your Public IP Address on :xxx.xxx.xxx.xxx
+def retKML(dstip, srcip):
+    dst = gi.record_by_name(dstip)
+    src = gi.record_by_name('xxx.xxx.xxx.xxx') 
+    try:
+        dstlongitude = dst['longitude']
+        dstlatitude = dst['latitude']
+        srclongitude = src['longitude']
+        srclatitude = src['latitude']
+        kml = (
+            '<Placemark>\n'
+            '<name>%s</name>\n'
+            '<extrude>1</extrude>\n'
+            '<tessellate>1</tessellate>\n'
+            '<styleUrl>#transBluePoly</styleUrl>\n'
+            '<LineString>\n'
+            '<coordinates>%6f,%6f\n%6f,%6f</coordinates>\n'
+            '</LineString>\n'
+            '</Placemark>\n'
+        )%(dstip, dstlongitude, dstlatitude, srclongitude, srclatitude)
+        return kml
+    except:
+        return ''
+  
+  
+def main():
+    f = open('wire.pcap', 'rb')
+    pcap = dpkt.pcap.Reader(f)
+    kmlheader = '<?xml version="1.0" encoding="UTF-8"?> \n<kml xmlns="http://www.opengis.net/kml/2.2">\n<Document>\n'\
+    '<Style id="transBluePoly">' \
+                '<LineStyle>' \
+                '<width>1.5</width>' \
+                '<color>501400E6</color>' \
+                '</LineStyle>' \
+                '</Style>'
+    kmlfooter = '</Document>\n</kml>\n'
+    kmldoc = kmlheader + plotIPs(pcap) + kmlfooter
+    
+    # Write KML data into a file
+    with open('output.kml', 'w') as kml_file:
+        kml_file.write(kmldoc)
+
+if __name__ == '__main__':
+    main()
